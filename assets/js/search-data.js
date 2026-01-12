8275614c74dbb03efa8edb8fefa36f6ac09dd2f9
jamie-rice <109659510+jamie-rice@users.noreply.github.com>
2026-01-12 05:11:18 +0000

    Deploying to gh-pages from @ jamie-rice/jamie-rice.github.io@75a53112263485765e00338d1be1aa1660abdb26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-ctfs",
+          title: "ctfs",
+          description: "Write ups of the CTFs I have found to be more interesting",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/ctfs/";
           },
         },{id: "nav-repositories",
           title: "repositories",
