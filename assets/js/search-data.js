950f9173e921d71ed72508452f0cee914b55b6b1
jamie-rice <109659510+jamie-rice@users.noreply.github.com>
2026-01-12 05:48:11 +0000

    Deploying to gh-pages from @ jamie-rice/jamie-rice.github.io@169e902b260fad5237100ae1b499543b58bf8758 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,48 +17,13 @@ ninja.data = [{
             window.location.href = "/blog/";
           },
         },{id: "nav-ctfs",
-          title: "ctfs",
-          description: "Write ups of the CTFs I have found to be more interesting",
+          title: "CTFs",
+          description: "Here are all the write-ups for the CTFs I find to be interesting. If you are looking for other security related writings please look at the blog.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/ctfs/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
